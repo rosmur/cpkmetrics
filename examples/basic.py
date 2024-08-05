e61b1ddfc4dc68ea4e6f8fc1bf1252c16f9c9c59
Ros M <rosmur@users.noreply.github.com>
2024-08-04 22:54:03 -0700

    updated values to most basic form, centered mean in tol window, even numbers, resulting in 1.33 Cpk

--- examples/basic.py
+++ examples/basic.py
@@ -1,10 +1,10 @@
 from process_capability import ProcessCapability
 
 # Example values for mean, stddev, USL, and LSL
-mean = 9
-stddev = 0.2
-USL = 12.0
-LSL = 8.0
+mean = 10
+stddev = 1
+USL = 14
+LSL = 6
 
 # Create an instance of the ProcessCapability class with the provided parameters. Instantiation also automatically prints results to the terminal
 pc = ProcessCapability(mean, stddev, USL, LSL)
