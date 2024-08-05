0d5535d8fd9b48cf22812b585d721e6bf08071cd
Roshan M <rillini@gmail.com>
2024-08-04 21:12:31 -0700

    Added printing an attribute

--- examples/basic.py
+++ examples/basic.py
@@ -6,5 +6,8 @@ stddev = 0.2
 USL = 12.0
 LSL = 8.0
 
-# Create an instance of the ProcessCapability class with the provided parameters
+# Create an instance of the ProcessCapability class with the provided parameters. Instantiation also automatically prints results to the terminal
 pc = ProcessCapability(mean, stddev, USL, LSL)
+
+# The calculated metrics are available (as a dictionary):
+print(pc.metrics)
