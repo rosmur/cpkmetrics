6dab74ccdd8653d4fe08727da2f647cb327c4f0b
Roshan M <rillini@gmail.com>
2024-08-25 23:12:55 -0700

    Added single value access example to basic.py (deprecated single_values example)

--- examples/basic.py
+++ examples/basic.py
@@ -1,4 +1,4 @@
-from process_capability import ProcessCapability
+from src.process_capability import ProcessCapability
 
 # Example values for mean, stddev, USL, and LSL
 mean = 10
@@ -6,5 +6,10 @@ stddev = 1
 USL = 14
 LSL = 6
 
-# Create an instance of the ProcessCapability class with the provided parameters. Instantiation also automatically prints results to the terminal
+# Create an instance of the ProcessCapability class with the provided parameters. Instantiation also automatically prints results to the terminal (unless you pass print_results=False arg)
 pc = ProcessCapability(mean, stddev, USL, LSL)
+
+# You can also access individual items directly through the corrresponding property
+cpk = pc.process_capability_index
+
+print(f"The Cpk is {round(cpk,2)}")
