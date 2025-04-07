fdba12bb437c3418377218b58a2fad1feb27c162
Ros M <rosmur@users.noreply.github.com>
2025-04-07 06:18:10 -0700

    update import statements for new folder structure

--- examples/basic.py
+++ examples/basic.py
@@ -1,4 +1,4 @@
-from src.process_capability import ProcessCapability
+from src.cpkmetrics.process_capability import ProcessCapability
 
 # Example values for mean, stddev, USL, and LSL
 mean = 10
@@ -12,4 +12,4 @@ pc = ProcessCapability(mean, stddev, USL, LSL)
 # You can also access individual items directly through the corrresponding property
 cpk = pc.process_capability_index
 
-print(f"The Cpk is {round(cpk,2)}")
+print(f"The Cpk is {round(cpk, 2)}")
