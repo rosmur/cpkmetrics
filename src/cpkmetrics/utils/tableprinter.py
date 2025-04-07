3302694696584d48bfb15a9b3e72a5ae019827d3
Roshan M <rillini@gmail.com>
2025-04-07 09:17:44 -0700

    update module docstrings

--- src/cpkmetrics/utils/tableprinter.py
+++ src/cpkmetrics/utils/tableprinter.py
@@ -1,4 +1,18 @@
+"""
+Table Printer Utility
+
+A lightweight utility to enable a pretty format for printing out metrics. Currently can be accomplished in single function. Roadmap for expanding this out.
+
+"""
+
+
 def print_table(data):
+    """Prints a table of metrics and their values to the console.
+
+    Args:
+        data (dict): A dictionary where keys are metric names and values are metric values.
+            Values that are floats will be formatted to 3 decimal places.
+    """
     col_names = ["Metric", "Value"]
 
     # Convert dictionary to list of lists, formatting floats to 3 decimal place
@@ -9,9 +23,7 @@ def print_table(data):
         table_data.append([k, v])
 
     # Find the maximum width for each column
-    col_widths = [
-        max(len(str(item)) for item in col) for col in zip(*table_data, col_names)
-    ]
+    col_widths = [max(len(str(item)) for item in col) for col in zip(*table_data, col_names)]
 
     # Create the format string
     row_format = "| {:<" + str(col_widths[0]) + "} | {:<" + str(col_widths[1]) + "} |"
