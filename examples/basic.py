82660a875d4c713c0d2c1525c01d82d2a0b0d911
Roshan M <rillini@gmail.com>
2025-06-23 15:42:26 -0700

    add more documentation to basic.py example

--- examples/basic.py
+++ examples/basic.py
@@ -1,12 +1,23 @@
-from src.cpkmetrics.process_capability import ProcessCapability
+"""
+A "Hello World" example of using the cpkmetrics package.
+
+This example shows calculation of Cpk for a given input of mean, standard deviation and spec limits.
+
+----
+NOTES:
+    - cpkmetrics package should be installed prior to use
+"""
+
+from cpkmetrics.process_capability import ProcessCapability
 
 # Example values for mean, stddev, USL, and LSL
-mean = 10
-stddev = 1
-USL = 14
-LSL = 6
+mean = 10  # Average
+stddev = 1  # Standard Deviation
+USL = 14  # Upper Spec Limit
+LSL = 6  # Lower Spec Limit
 
-# Create an instance of the ProcessCapability class with the provided parameters. Instantiation also automatically prints results to the terminal (unless you pass print_results=False arg)
+# Create an instance of the ProcessCapability class with the provided parameters.
+# Instantiation also automatically prints results to the terminal (the print_results arg defaults to True)
 pc = ProcessCapability(mean, stddev, USL, LSL)
 
 # You can also access individual items directly through the corrresponding property
